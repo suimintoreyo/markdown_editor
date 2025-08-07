7251a6f42dd94ea421549d458e2db1cfc8e70b10
suimintoreyo <166894717+suimintoreyo@users.noreply.github.com>
2025-08-07 15:57:32 +0900

    refactor: extract JS syntax highlighting

--- script.js
+++ script.js
@@ -14,29 +14,6 @@ function escapeHtml(text) {
   return text.replace(/[&<>]/g, (char) => map[char]);
 }
 
-// JavaScriptの構文をハイライトする
-function highlightJsSyntax(code) {
-  code = code.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-
-  const keywordPattern = `\\b(?:function|const|let|var|if|else|return|for|while|do|switch|case|break|continue)\\b`;
-  const regex = new RegExp(
-    `(".*?")|('.*?')|(\\/\\/.*)|(\\/\\*[\\s\\S]*?\\*\\/)|(${keywordPattern})|(\\b\\d+\\b)|(\\b[a-zA-Z_]\\w*(?=\\s*\\())`,
-    "g"
-  );
-
-  return code.replace(
-    regex,
-    (match, string1, string2, comment1, comment2, keyword, number, funcName) => {
-      if (string1 || string2) return `<span class="string">${match}</span>`;
-      if (comment1 || comment2) return `<span class="comment">${match}</span>`;
-      if (keyword) return `<span class="keyword">${match}</span>`;
-      if (number) return `<span class="number">${match}</span>`;
-      if (funcName) return `<span class="function-name">${match}</span>`;
-      return match;
-    }
-  );
-}
-
 // 強調、コード、画像、リンクなどのインラインMarkdown機能を適用する。
 function applyInlineFormatting(text) {
   return (
